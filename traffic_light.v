b6ef8e3360818f10336b138b826944b9a478ac54
ouyangxiangqian <norra1@zoho.com.cn>
2024-11-23 21:55:38 +0800

    near complete

--- traffic_light.v
+++ traffic_light.v
@@ -1,9 +1,9 @@
 module traffic_light(
 	input			sys_clk,
-	input			sys_rst_n,
+	input			sys_rst_p,
 	input			sys_clk_1s,
-	output	[3:0]	light_t,
-	output	[2:0]	light_ctrl
+	output	reg [3:0]	light_t,
+	output	reg [2:0]	light_ctrl
 	);
 /* PARAM */
 // Global Parameter
@@ -11,10 +11,10 @@ parameter [2:0]   G_T  = 'd10,
 				  Y_T  = 'd5,
 				  R_T  = 'd15;
 // Local Parameter
-localparam [3:0]  IDLE = 3'd0,
-				  G    = 3'd1,
-				  Y    = 3'd2,
-				  R    = 3'd3;
+localparam [3:0]  IDLE = 4'b0001,
+				  G    = 4'b0010,
+				  Y    = 4'b0100,
+				  R    = 4'b1000;
 
 /* local var */
 reg [3:0] cur_st;
@@ -22,8 +22,8 @@ reg [3:0] nxt_st;
 
 /* State Machine Part */
 // First Machine: State Register
-always @(posedge sys_clk or posedge sys_rst_n) begin
-	if (sys_rst_n) begin
+always @(posedge sys_clk or posedge sys_rst_p) begin
+	if (sys_rst_p) begin
 		// reset
 		cur_st <= IDLE;
 	end
@@ -32,15 +32,47 @@ always @(posedge sys_clk or posedge sys_rst_n) begin
 	end
 end
 // Second Machine: State Transfer
-always @(cur_st or sys_rst_n or light_t) begin
-if (sys_rst_n) begin
+always @(*) begin
+if (sys_rst_p) begin
 	nxt_st <= IDLE;
 end
 	case(cur_st)
 		IDLE	:nxt_st<=(light_t==4'd1)?G:IDLE;
-		G 		:
-		Y 		:
-		R  		:
+		G 		:nxt_st<=(light_t==4'd1)?Y:G;
+		Y 		:nxt_st<=(light_t==4'd1)?R:Y;
+		R  		:nxt_st<=(light_t==4'd1)?G:R;
+		default :nxt_st<=IDLE;
 	endcase
+end
+// Third Machine: Output Control for next state
+always @(posedge sys_clk_1s or posedge sys_rst_p) begin
+if (sys_rst_p) begin
+	light_ctrl <= 3'b001;
+	light_t    <= G_T;
+end
+    case(nxt_st)
+        IDLE:begin
+	            light_ctrl <= 3'b000;
+	            if (light_t=='d1) light_t<=G_T;
+	            else light_t<=light_t-1;
+        	end
+        G 	:begin
+	            light_ctrl <= 3'b001;
+	            if (light_t=='d1) light_t<=Y_T;
+	            else light_t<=light_t-1;
+        	end
+        Y 	:begin
+	            light_ctrl <= 3'b010;
+	            if (light_t=='d1) light_t<=R_T;
+	            else light_t<=light_t-1;
+        	end
+        R 	:begin
+	            light_ctrl <= 3'b100;
+	            if (light_t=='d1) light_t<=G_T;
+	            else light_t<=light_t-1;
+        	end
+        default:
+            light_ctrl <= 3'b000;
+    endcase
 end
 endmodule
