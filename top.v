3d9bcb433d071108b0cde43b868ba78b790af78f
ouyangxiangqian <norra1@zoho.com.cn>
2024-11-24 15:37:43 +0800

    first sys

--- top.v
+++ top.v
@@ -7,18 +7,18 @@ module top(
 	);
 
 wire sys_clk_1hz;
-wire [3:0] carry_light_t;
+wire [15:0] carry_light_t;
 
 clk_div_1Hz #(.DIVISOR(26'd50000000)) inst_clk_div_1Hz (
 	.sys_clk(sys_clk), 
 	.sys_rst_p(sys_rst_p), 
-	.clk_1hz(clk_1hz)
+	.clk_1hz(sys_clk_1hz)
 	);
-traffic_light_optimized inst_traffic_light_optimized
+traffic_light inst_traffic_light_optimized
 	(
 		.sys_clk    (sys_clk),
 		.sys_rst_p  (sys_rst_p),
-		.sys_clk_1s (sys_clk_1s),
+		.sys_clk_1s (sys_clk_1hz),
 		.light_t    (carry_light_t),
 		.light_ctrl (light_ctrl)
 	);
